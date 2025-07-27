84b39e299b2e99e60d001cbcb0edd33293c58c23
shubham <shubhpatil8510@gmail.com>
2025-07-27 13:23:20 +0530

    Created a header component

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,17 @@
+import { Header } from "~/components"
+
 const Dashboard = () => {
+  const user = {
+    name : "Shubham",
+
+  }
   return (
-    <div>Dashboard Page</div>
+    <main className="dashboard wrapper">
+        <Header 
+          title = {`Welcome ${user.name ?? 'Guest'}, 👋`}
+          description = "Track activity, trends and populer destination in real time"
+        />
+    </main>
   )
 }
 
